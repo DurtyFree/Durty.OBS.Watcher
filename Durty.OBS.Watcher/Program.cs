249ac1b7ecb83b7fa4b393468aa96223e89bf1cb
DurtyFree <schmid.alexander@outlook.de>
2019-11-21 00:18:07 +0100

    Add capture window with tooltips mode idea

--- Durty.OBS.Watcher/Program.cs
+++ Durty.OBS.Watcher/Program.cs
@@ -1,24 +1,33 @@
 ﻿using System;
+using System.Linq;
 using Durty.OBS.Watcher.Handlers;
 using Durty.OBS.Watcher.Repositories;
 
 namespace Durty.OBS.Watcher
 {
     /* TODO / IDEAS
-     - Debug Mode to diagnose issues
-     - On startup check all defined actions if scene & source names are valid
-     - All actions should be defined in json files
-     - Application configuration should be outsourced to configuration json
+    - Debug Mode to diagnose issues
+    - On startup check all defined actions if scene & source names are valid
+    - All actions should be defined in json files
+    - Application configuration should be outsourced to configuration json
+    - Maybe consider porting to .NET Console project, because of OBS /OBS WebSockets dependency & focused window change API
 
-     - Focused Window Change title should support placeholders 'DurtyMapEditor%Visual Studio' to check for multiple contain or exact window title match
+    - Focused Window Change title should support placeholders 'DurtyMapEditor%Visual Studio' to check for multiple contain or exact window title match
 
-     - Change Scene Action on focused window changed
-     - Optional only enabled when in specific scene currently
+    - Change Scene Action on focused window changed
+    -> Optional only enabled when in specific scene currently
 
-     - Change Scene Source Render/Visibility on focused window changed
-     - Optional only enabled when in specific scene currently
-     - Only render source when specified window is focused
-     - Support changing source render toggle for all available scenes, not only current one
+    - Change Scene Source Render/Visibility on focused window changed
+    -> Optional only enabled when in specific scene currently
+    -> Only render source when specified window is focused
+    -> Support changing source render toggle for all available scenes, not only current one
+
+    - Capture Window with tooltips mode
+    -> Creates / Uses display capture on monitor with defined window
+    -> Toggles Display Capture source visibility whenever defined window is in focus
+    -> When defined window is out of focus, hide display capture & show window capture
+    -> Only change to display capture if defined window is focused for more than x seconds? (Safety / fast switch protection)
+    -> Auto raise focused window watcher polling?
      */
 
     class Program
